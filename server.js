7a7c6799c44c54953f6a913131ebfab8205c449b
Raj Chavda <rajchavda463@gmail.com>
2026-05-12 11:45:50 +0530

    cors error solve

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "http://localhost:3001"
+        "http://localhost:3001",
+        "http://localhost:3002"
     ],
 
 }));
