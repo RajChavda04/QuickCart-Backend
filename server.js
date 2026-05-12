835656873f4d0c6948026279a9e372273063b564
Raj Chavda <rajchavda463@gmail.com>
2026-05-12 14:11:50 +0530

    remove import line from server

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ dotenv.config({
 })
 
 const PORT = process.env.PORT || 1337
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(express.json());
 app.use(cors({
